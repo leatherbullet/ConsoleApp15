d44dc2e78e2e62cc1ce262eff4c31469b56c9abb
leatherbullet <al.obnizov@yandex.ru>
2024-03-03 10:17:45 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int minValue = 50;
+            int maxValue = 150;
+            int rangeMin = 10;
+            int rangeMax = 26;
+            int number = 0;
+            int randomNumber = random.Next(rangeMin, rangeMax);
+            Console.WriteLine("случайное число " + randomNumber);
+
+            for (int i = 0; i <= maxValue; i += randomNumber)
+            { 
+                if (i >= minValue)
+                {
+                    ++number;
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("количество кратных чисел: " + number);
+        }
+    }
+}
